17ee333d47baea5891ad481fe2f0f757b76a96f9
Boyd Evans III <bbeiii@yahoo.com>
2021-07-13 10:29:58 -0600

    adding URL's

---
+++ static/js/logic.js
@@ -0,0 +1,4 @@
+// Set URL Variables
+var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var plate_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
