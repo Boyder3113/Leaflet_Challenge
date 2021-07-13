174cb362a50e4666329a380fbdf0102dc233aae8
Boyd Evans III <bbeiii@yahoo.com>
2021-07-13 10:39:25 -0600

    adding marker sizing

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,3 +2,7 @@
 var earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var plate_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
+// set marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 4;
+};
