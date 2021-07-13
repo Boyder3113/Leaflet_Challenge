a15846ec69f78798e1e5031a2f1ca3876b4563b7
Boyd Evans III <bbeiii@yahoo.com>
2021-07-13 11:09:41 -0600

    adding earthquakeweek layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,3 +6,30 @@ var plate_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/
 function markerSize(magnitude) {
     return magnitude * 4;
 };
+
+var earthquakeweek = new L.LayerGroup();
+
+d3.json(earthquake_url, function (geoJson) {
+    L.geoJSON(geoJson.features, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            return L.circleMarker(latlng, { radius: markerSize(geoJsonPoint.properties.mag) });
+        },
+
+        style: function (geoJsonFeature) {
+            return {
+                fillColor: Color(geoJsonFeature.properties.mag),
+                fillOpacity: 0.7,
+                weight: 0.1,
+                color: 'black'
+
+            }
+        },
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "<h4 style='text-align:center;'>" + new Date(feature.properties.time) +
+                "</h4> <hr> <h5 style='text-align:center;'>" + feature.properties.title + "</h5>");
+        }
+    }).addTo(earthquakeweek);
+    createMap(earthquakeweek);
+});
